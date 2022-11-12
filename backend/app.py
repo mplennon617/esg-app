53df1341f861efaf9320e35e2e1802f74287b575
Adwoa Asare <robotics.mags@gmail.com>
2022-11-12 13:14:01 -0600

    adding example API calls to test

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import finnhub
 from flask import Flask
 app = Flask(__name__)
 
@@ -7,3 +8,12 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+#API ket for finnhub.io
+finnhub_client = finnhub.Client(api_key="cdo24g2ad3i5o5oksvlgcdo24g2ad3i5o5oksvm0")
+print("Recommendation Trends:\n")
+print(finnhub_client.recommendation_trends('AAPL'))
+print("Quote:\n")
+print(finnhub_client.quote('AAPL'))
+print("Financials as Reported:\n")
+print(finnhub_client.financials_reported(symbol='AAPL', freq='annual'))
