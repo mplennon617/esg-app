4bd1b298899f8219c89b3380c2004268fabcba72
root <root@DESKTOP-HQA5VVK.localdomain>
2022-11-12 19:43:50 -0600

    Remove firebase config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
+# Initialize Flask
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
+# Import routes
+from app import routes
 
+# Run the application
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
