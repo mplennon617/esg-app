d301732efba3ae503892847a416d86747bab23e8
Adwoa Asare <robotics.mags@gmail.com>
2022-11-13 10:41:50 -0600

    fixed recommendationtrends and fiinancialsreported. Quote might be fixed, but it also may be getting an empty value

--- backend/app.py
+++ backend/app.py
@@ -13,30 +13,49 @@ def hello_world():
 @app.route('/profiles/<company>')
 def CompanyProfile(company):
     ticker = finnhub_client.symbol_lookup(company) #get company ticker
-    profile = "{"+str(RecommendationTrends(ticker))+","+str(Quote(ticker))+","+str(FinancialsReported(ticker))+"}"
+    profile = "{"+str(RecommendationTrends(ticker))+","+str(FinancialsReported(ticker))+","+str(Quote(ticker))+"}"
     return profile
     
 def Quote(tick):
     #parse quotes for current stock price
-    pythonObj = json.loads(str(finnhub_client.quote(tick)))
+    quo = str(finnhub_client.quote(tick))
+    quo = quo.replace("'","\"")
+    pythonObj = json.loads(quo)
     currentPrice = pythonObj['c']
     return "\"CurrentPrice\":"+str(currentPrice)
 
 def FinancialsReported(tick):
     #parse financials as reported to get assets and gross profits
-    pythonObj = json.loads(str(finnhub_client.financials_reported(symbol=tick, freq='annual')))
-    assets = pythonObj['report']['bs']['Assets']
-    liability = pythonObj['report']['bs']['Liabilities']
-    revenue = pythonObj['report']['bs']['Revenues']
-    grossProfits = pythonObj['report']['ic']['GrossProfit']
-    earningPerShare = pythonObj['report']['ic']['EarningsPerShareBasic']
-    console.log(pythonObj)
-    date = pythonObj['year']
+    finRep = str(finnhub_client.financials_reported(symbol=tick, freq='annual', _from="2020-01-01", _to="2021-01-01"))
+    finRep = finRep.replace("'","\"")
+    pythonObj = json.loads(finRep)
+    n=0
+    assets = []
+    liability=[]
+    revenue=[]
+    grossProfits=[]
+    earningsPerShare =[]
+    for item in pythonObj['data']:
+        n = n+1
+        assets.append(item['report']['bs']['Assets'])
+        liability.append(item['report']['bs']['Liabilities'])
+        grossProfits.append(item['report']['ic']['GrossProfit'])
+        earningsPerShare.append(item['report']['ic']['EarningsPerShareBasic'])
+    
+    #assets = pythonObj['report']['bs']['Assets']
+    #liability = pythonObj['report']['bs']['Liabilities']
+    #revenue = pythonObj['report']['bs']['Revenues']
+    #grossProfits = pythonObj['report']['ic']['GrossProfit']
+    #earningPerShare = pythonObj['report']['ic']['EarningsPerShareBasic']
+    #console.log(pythonObj)
+    #date = pythonObj['year']
     return "\"Assets\":"+str(assets)+",\"Revenues\":"+str(revenue)+",\"Liabilities\":"+str(liability)+",\"GrossProfit\":"+str(grossProfits)
 
 def RecommendationTrends(tick):
     #parse exert recomendation trends for recent periods
-    pythonObj = json.loads(str(finnhub_client.recommendation_trends(tick)))
+    recTrends = str(finnhub_client.recommendation_trends(tick))
+    recTrends = recTrends.replace("'","\"")
+    pythonObj = json.loads(recTrends)
     return "\"Recommendations\":"+str(pythonObj)
 
 def EnvironmentalFactors(Company):
